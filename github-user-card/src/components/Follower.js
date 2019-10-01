c16e735c7632599ae44241d7f88ec42c46a25f37
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-01 17:19:12 +0100

    all data displaying and preliminary styling added

--- github-user-card/src/components/Follower.js
+++ github-user-card/src/components/Follower.js
@@ -2,12 +2,11 @@ import React from "react";
 
 class Follower extends React.Component {
   render() {
-    debugger
+    console.log(this.props);
     return (
       <div key={this.props.id}>
-        <h2>Followers:</h2>
-        <p>Username: {this.props.followers.login}</p>
-        {/* <img src={this.props.followers.avatar_url} alt={this.props.followers.name}></img> */}
+        <p>Username: {this.props.login}</p>
+        <img src={this.props.image} alt={this.props.name} /> 
       </div>
     );
   }
--- github-user-card/src/components/FollowersList.js
+++ github-user-card/src/components/FollowersList.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Follower from "./Follower";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Styledh2 = styled.h2`
+  font-size: 30px;
+  text-align: center;
+`;
 
 class FollowersList extends React.Component {
   render() {
-    return this.props.followers.map(follower => (
-      <Follower
-        key={follower.id}
-        image={follower.avatar_url}
-        login={follower.login}
-      />
-    ));
+    return (
+      <div>
+        <Styledh2>Followers:</Styledh2>
+        <StyledDiv>
+          {this.props.followers.map(follower => (
+            <Follower
+              key={follower.id}
+              image={follower.avatar_url}
+              login={follower.login}
+            />
+          ))}
+        </StyledDiv>
+      </div>
+    );
   }
 }
 
